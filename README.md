f5cb598ff9b224fbeff9f834fe6a99946d9fad82
beech <beechmanbopp@gmail.com>
2024-12-14 03:18:18 -0600

    Finalizing

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Alpine-Config
 
 Quick configuration for Alpine Linux. Setting up docker & community repository.
+
+---
+
+```bash
+# Install Curl
+doas apk update
+doas apk add curl
+
+# Download & Run Script
+curl https://git.merlinslair.net/beech/Alpine-Config/raw/branch/main/setup.sh -o setup.sh
+chmod +x setup.sh
+sh setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Enabling Community Repository..."
+echo "Enter Root Password."
 su -c 'echo "http://dl-cdn.alpinelinux.org/alpine/v3.21/community" >> /etc/apk/repositories'
 echo "Enabled. Updating & Installing Docker"
 
+echo "Enter User Password."
 doas apk update
 doas apk add docker
 doas apk add docker-cli-compose
