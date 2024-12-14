9173d6e20d5f3dbe675f714ca63c4b82571d5076
beech <beechmanbopp@gmail.com>
2024-12-14 02:59:52 -0600

    Finalizing

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@
 
 echo "Enabling Community Repository..."
 su -c 'echo "http://dl-cdn.alpinelinux.org/alpine/v3.21/community" >> /etc/apk/repositories'
+echo "Enabled. Updating & Installing Docker"
 
 doas apk update
 doas apk add docker
 doas apk add docker-cli-compose
+doas rc-update add docker default
 doas service docker start
 doas addgroup ${USER} docker
 
-echo "All setups complete."
+echo "All setups complete. Please reboot."
