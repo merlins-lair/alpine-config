ead4cb5c83459711ced754b9c0130893f695d9ec
beech <beechmanbopp@gmail.com>
2024-12-14 02:48:29 -0600

    Community repository

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Enabling Community Repository..."
-sed -i 's/^#https://dl-cdn.alpinelinux.org/alpine/v3.18/community/https://dl-cdn.alpinelinux.org/alpine/v3.18/community/' /etc/apk/repositories
+doas echo "http://dl-cdn.alpinelinux.org/alpine/v3.21/community" >> /etc/apk/repositories
 
 doas apk update
 doas apk add docker
