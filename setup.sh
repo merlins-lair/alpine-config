36541d4031b09ad01baf5a75e0335cd21e43fa05
beech <beechmanbopp@gmail.com>
2024-12-14 02:52:24 -0600

    Start docker service

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ su -c 'echo "http://dl-cdn.alpinelinux.org/alpine/v3.21/community" >> /etc/apk/r
 doas apk update
 doas apk add docker
 doas apk add docker-cli-compose
+doas service docker start
 doas addgroup ${USER} docker
 
 echo "All setups complete."
